df6c6a83d3a13530c71e73f4df125e7bfe8190b5
afermon <alex.fermon@outlook.com>
2019-02-08 20:52:37 -0600

    JWT

--- src/main/java/com/cosmicode/roomie/security/AuthoritiesConstants.java
+++ src/main/java/com/cosmicode/roomie/security/AuthoritiesConstants.java
@@ -11,6 +11,12 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String FACEBOOK = "ROLE_USER_FACEBOOK";
+
+    public static final String GOOGLE = "ROLE_USER_GOOGLE";
+
+    public static final String PRO = "ROLE_USER_PRO";
+
     private AuthoritiesConstants() {
     }
 }
