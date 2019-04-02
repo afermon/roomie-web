2a8f999391509ce9f2f4238e91ef7fdec6b9d75b
afermon <alex.fermon@outlook.com>
2019-04-02 17:37:12 -0600

    mapping

--- src/main/java/com/cosmicode/roomie/service/mapper/AppointmentMapper.java
+++ src/main/java/com/cosmicode/roomie/service/mapper/AppointmentMapper.java
@@ -12,6 +12,7 @@ import org.mapstruct.Mapping;
 public interface AppointmentMapper extends EntityMapper<AppointmentDTO, Appointment> {
 
     @Mapping(source = "petitioner.id", target = "petitionerId")
+    @Mapping(source = "petitioner.id", target = "petitioner")
     @Mapping(target = "room")
     @Mapping(source = "room.id", target = "roomId")
     AppointmentDTO toDto(Appointment appointment);
